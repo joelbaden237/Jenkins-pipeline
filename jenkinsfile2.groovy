0f765e330b71d96b5f68ec521b5cdf262c9bb9f9
joelbaden237 <s219422682@deakin.edu.au>
2025-05-16 15:56:04 +1000

    Update Stages

--- jenkinsfile2.groovy
+++ jenkinsfile2.groovy
@@ -3,55 +3,37 @@ pipeline{
     stages{
         stage("Build"){
             steps{
-                echo "Build the code using a build automation tool to compile and package
-                your code. You need to specify at least one build automation tool, e.g., Maven."
-            }
-            post{
-                always{
-                    mail to: "jbaden777@gmail.com",
-                    subject: "Build Status Email",
-                    body: "Build Log Attached!"
-                }
+                echo "Using Maven to compile and package the code"
             }
         }
         stage("Unit and Integration Tests"){
             steps{
-                echo "Run unit tests to ensure the code functions as
-                      expected and run integration tests to ensure the different components of the
-                      application work together as expected. You need to specify test automation tools for
-                      this stage."
+                echo "Using JUnit for unit testing and Selenium for integration testing."
             }
         }
          stage("Code Analysis"){
             steps{
-                echo "Integrate a code analysis tool to analyse the code and ensure
-                      it meets industry standards. Research and select a tool to analyse your code using
-                      Jenkins"
+                echo "Using SonarQube for code analysis."
             }
          }
          stage("Security Scan"){
             steps{
-                echo "Perform a security scan on the code using a tool to identify
-                      any vulnerabilities. Research and select a tool to scan your code."
+                echo "Using OWASP to identify vulnerabilities in code."
             }
          }
         stage("Deploy to Staging"){
             steps{
-                echo "Deploy the application to a staging server (e.g., AWS EC2
-                      instance)."
+                echo "Deploying the application to AWS EC2 staging server."
             }
          }
          stage("Integration Tests on Staging"){
             steps{
-                echo "Run integration tests on the staging
-                        environment to ensure the application functions as expected in a production-like
-                        environment."
+                echo "Running integration tests on the staging environment such as CI/CD Smoke Test."
             }
          }
          stage("Deploy to Production"){
             steps{
-                echo "Deploy the application to a production server (e.g.,
-                      AWS EC2 instance)."
+                echo "Deploying the application to AWS EC2 production server."
             }
          }
         
