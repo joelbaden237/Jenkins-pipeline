254d5d5cbbd0acd6ed0c99e799c20df6fdc37dbc
joelbaden237 <s219422682@deakin.edu.au>
2025-05-16 15:40:38 +1000

    Add 7 Stages to Jenkins

--- jenkinsfile2.groovy
+++ jenkinsfile2.groovy
@@ -3,36 +3,58 @@ pipeline{
     stages{
         stage("Build"){
             steps{
-                echo "Building ..."
+                echo "Build the code using a build automation tool to compile and package
+                your code. You need to specify at least one build automation tool, e.g., Maven."
             }
             post{
                 always{
                     mail to: "jbaden777@gmail.com",
                     subject: "Build Status Email",
                     body: "Build Log Attached!"
-
                 }
             }
-
         }
-        stage("Test"){
+        stage("Unit and Integration Tests"){
             steps{
-                echo "Testing..."
+                echo "Run unit tests to ensure the code functions as
+                      expected and run integration tests to ensure the different components of the
+                      application work together as expected. You need to specify test automation tools for
+                      this stage."
             }
-
-
-
         }
-         stage("Deploy"){
+         stage("Code Analysis"){
             steps{
-                echo "Deploying..."
+                echo "Integrate a code analysis tool to analyse the code and ensure
+                      it meets industry standards. Research and select a tool to analyse your code using
+                      Jenkins"
             }
          }
-         stage("Complete"){
+         stage("Security Scan"){
             steps{
-                echo "Completed..."
+                echo "Perform a security scan on the code using a tool to identify
+                      any vulnerabilities. Research and select a tool to scan your code."
             }
          }
+        stage("Deploy to Staging"){
+            steps{
+                echo "Deploy the application to a staging server (e.g., AWS EC2
+                      instance)."
+            }
+         }
+         stage("Integration Tests on Staging"){
+            steps{
+                echo "Run integration tests on the staging
+                        environment to ensure the application functions as expected in a production-like
+                        environment."
+            }
+         }
+         stage("Deploy to Production"){
+            steps{
+                echo "Deploy the application to a production server (e.g.,
+                      AWS EC2 instance)."
+            }
+         }
+        
     
 
 
